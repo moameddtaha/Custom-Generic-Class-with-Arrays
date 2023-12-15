bde6f4d313c0d0ad7a61d68927a759b599d37ca0
Mohamed Taha <Panda@gmail.com>
2023-12-15 10:14:42 +0200

    Add project files.

---
+++ Custom Generic Class with Arrays/Database.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Generic_Class_with_Arrays
+{
+    public class Database<TKey, TValue>
+    {
+        private TKey[] keys;
+        private TValue[] values;
+        private int count;
+
+        public Database()
+        {
+            keys = new TKey[4];
+            values = new TValue[4];
+            count = 0;
+        }
+        public Database(TKey[] keys, TValue[] values, int count)
+        {
+            this.keys = keys;
+            this.values = values;
+            this.count = count;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (count == keys.Length)
+            {
+                Array.Resize(ref keys, keys.Length * 2);
+                Array.Resize(ref values, values.Length * 2);
+            }
+            keys[count] = key;
+            values[count] = value;
+            count++;
+        }
+
+        public bool Remove(TKey key)
+        {
+            int index = Array.IndexOf(keys, key);
+            if(index == -1) return false;
+
+            for (int i = index; i < count - 1; i++)
+            {
+                keys[i] = keys[i + 1];
+                values[i] = values[i + 1];
+            }
+            count--;
+
+            if (count < keys.Length / 4)
+            {
+                Array.Resize(ref keys, keys.Length / 2);
+                Array.Resize(ref values, values.Length / 2);
+            }
+
+            return true;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            int index = Array.IndexOf(keys, key);
+            if (index == -1)
+            {
+                value = default;
+                return false;
+            }
+            value = values[index];
+            return true;
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+    }
+}
---
+++ Custom Generic Class with Arrays/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Custom_Generic_Class_with_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Database<int, string> db = new Database<int, string>();
+
+            db.Add(1, "One");
+            db.Add(2, "Two");
+            db.Add(3, "Three");
+            db.Add(4, "Four");
+
+            Console.WriteLine("Before remove.");
+            Console.WriteLine($"Count {db.Count}");
+
+            string value;
+            if (db.TryGetValue(1, out value))
+            {
+                Console.WriteLine($"Value for key 2: {value}");
+            }
+
+            db.Remove(1);
+
+            Console.WriteLine("After remove.");
+            Console.WriteLine($"Count {db.Count}");
+
+            int a = 1, b = 2;
+            Swap(ref a, ref b);
+            Console.WriteLine($"a = {a}, b = {b}");
+
+            MyClass c1 = new MyClass(1, 10), c2 = new MyClass(2, 20);
+
+            Swap(ref c1, ref c2);
+
+            System.Console.WriteLine($"c1.Value = {c1.Id} c2.Value = {c2.Id}");
+
+        }
+
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T Temp = a;
+            a = b;
+            b = Temp;
+        }
+
+        class MyClass
+        {
+            public int Value { get; set; }
+            public int Id { get; set; }
+
+            public MyClass(int value, int id)
+            {
+                Value = value;
+                Id = id;
+            }
+        }
+
+    }
+}
